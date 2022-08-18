e96960facc9a390ad8fa088665fe585a2bd594bd
Vlad <vladmalpro37@gmail.com>
2022-08-18 17:58:02 +0300

    general info component

--- src/components/GeneralInfo.vue
+++ src/components/GeneralInfo.vue
@@ -1,51 +1,47 @@
 <template>
-<div class="container">
-      <div class="about-us">
-        <h1 class="about-us-title">про нас</h1>
-        <div class="about-us-desc">
-          <p class="about-us-text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-            dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
-          <img src="../assets/img/PCB-about.jpg" alt="PCB-about">
-        </div>
+  <div class="container">
+    <div class="about-us">
+      <h1 class="about-us-title">про нас</h1>
+      <div class="about-us-desc">
+        <p class="about-us-text">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
+        </p>
+        <img src="../assets/img/PCB-about.jpg" alt="PCB-about" />
       </div>
-
-      <h1 class="about-us-title">Ми пропонуємо</h1>
-      <ul class="we-offer">
-        <li class="offer-element">
-          <div class="offer-icon-container">
-            <img src="../assets/img/support.svg" alt="offer-icon">
-          </div>
-          <h5>консультація</h5>
-          <p>Ми пропонуємо консультацію щодо організації проекту та вибору можливих рішень</p>
-        </li>
-
-        <li class="offer-element">
-          <div class="offer-icon-container">
-            <img src="../assets/img/money-bag.svg" alt="offer-icon">
-          </div>
-          <h5>бюджетне рішення</h5>
-          <p>Наші прості та ергономічні розробки будуть по кишені кожному </p>
-        </li>
-
-        <li class="offer-element">
-          <div class="offer-icon-container">
-            <img src="../assets/img/clean-hands.svg" alt="offer-icon">
-          </div>
-          <h5>підтримка</h5>
-          <p>Ми надаємо тех-підтримку та обсуговування наших товарів під час есплуатації </p>
-        </li>
-      </ul>
     </div>
+
+    <h1 class="about-us-title">Ми пропонуємо</h1>
+    <ul class="we-offer">
+      <li class="offer-element" v-for="offer in offers" :key="offer.title">
+        <div class="offer-icon-container">
+          <img :src="require(`../assets/img/${offer.img_src}`)" alt="offer-icon" />
+        </div>
+        <h5>{{ offer.offer_title }}</h5>
+        <p>{{ offer.offer_desc }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'GeneralInfo',
+  name: "GeneralInfo",
   props: {
-    msg: String
-  }
-}
+  },
+  data: function () {
+    return {
+      offers: [
+        { img_src: "clean-hands.svg", offer_title: "консультація", offer_desc: "Ми пропонуємо консультацію щодо організації проекту та вибору можливих рішень" },
+        { img_src: "money-bag.svg", offer_title: "бюджетне рішення", offer_desc: "Наші прості та ергономічні розробки будуть по кишені кожному" },
+        { img_src: "clean-hands.svg", offer_title: "підтримка", offer_desc: "Ми надаємо тех-підтримку та обсуговування наших товарів під час есплуатації" },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,22 +11,22 @@
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
-import Banner from '@/components/Banner.vue';
-import GeneralInfo from '@/components/GeneralInfo.vue';
-import Products from '@/components/Products.vue';
-import Contacts from '@/components/Contacts.vue';
-import Footer from '@/components/Footer.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+import Banner from "@/components/Banner.vue";
+import GeneralInfo from "@/components/GeneralInfo.vue";
+import Products from "@/components/Products.vue";
+import Contacts from "@/components/Contacts.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     HelloWorld,
     Banner,
     GeneralInfo,
     Products,
     Contacts,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
